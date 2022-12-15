6b7de95b74cd7bad290052946a8d7e696a7276a8
Артём Климов <temni@mail.ru>
2022-12-15 16:42:34 +0700

    Рабочая версия, до задания с добавлением групп

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,23 @@
-from django.http import HttpResponse
+from .models import Post
+from django.shortcuts import render
 
 
 # Главная страница
-def index(request):    
-    return HttpResponse('ГЛАВНАЯ СТРАНИЦА')
+def index(request):
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'title': title,
+        'posts': posts
+    }
+    return render(request, template, context) 
 
 
 def group_posts(request, slug):
-    return HttpResponse('СТРАНИЦА GROUP_POSTS')
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title
+    }
+    return render(request, template, context) 
