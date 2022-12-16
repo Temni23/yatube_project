561d68dc858ad21de5d4a9eb42c21f467c81977b
Артём Климов <temni@mail.ru>
2022-12-16 15:08:41 +0700

    Рабочая версия перед отправкой

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,12 @@
-from .models import Post
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
 
 
 # Главная страница
 def index(request):
     template = 'posts/index.html'
-    title = 'Это главная страница проекта Yatube'
+    title = 'Последние обновления на сайте'
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'title': title,
@@ -15,9 +16,13 @@ def index(request):
 
 
 def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
     template = 'posts/group_list.html'
-    title = 'Здесь будет информация о группах проекта Yatube'
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = f'Записи сообщества {group.title}.'
     context = {
-        'title': title
+        'title': title,
+        'group': group,
+        'posts': posts,
     }
     return render(request, template, context) 
